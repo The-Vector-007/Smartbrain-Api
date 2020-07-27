9ed395f308fc7ff4a96b3cb2e17abc5e4ca8f7b2
aj709 <55025205+aj709@users.noreply.github.com>
2020-07-27 11:25:20 +0530

    Fix

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+
 const db = knex({
 	client: 'pg',
 	connection: {
