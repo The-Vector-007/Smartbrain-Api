3c86de596bfe467469d150916dd547f36e62a201
aj709 <55025205+aj709@users.noreply.github.com>
2020-07-27 11:17:33 +0530

    fixing

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const db = knex({
 app.use(express.json());
 app.use(cors());
 
-app.get('/',(req,res) => { res.send(db.users) });
+app.get('/',(req,res) => { res.send('It is Working') });
 app.post('/signin',(req,res)=>{ signin.handleSignin(req,res,db,bcrypt) });
 app.post('/register', (req,res)=>{ register.handleRegister(req,res,db,bcrypt) } );
 app.get('/profile/:id',(req,res)=>{ profile.handleProfileGet(req,res,db) });
