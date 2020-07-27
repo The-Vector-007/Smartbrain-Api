366dacc29a21f252c779d57d28fb9a545d525376
aj709 <55025205+aj709@users.noreply.github.com>
2020-07-27 11:05:17 +0530

    adding database

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
 	client: 'pg',
 	connection: {
-		host : '127.0.0.1',
-		user : 'postgres',
-		password : 'gm201000',
-		database : 'smartbrain'
+		connectString: process.env.DATABASE_URL,
+		ssl: true
 	}
 });
 
