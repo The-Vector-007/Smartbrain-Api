e09efce13a99578bf212b3c193aed0268e588015
aj709 <55025205+aj709@users.noreply.github.com>
2020-07-27 09:45:03 +0530

    Adding files

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const db = knex({
 app.use(express.json());
 app.use(cors());
 
-
+app.get('/',(req,res) => { res.send(db.users) });
 app.post('/signin',(req,res)=>{ signin.handleSignin(req,res,db,bcrypt) });
 app.post('/register', (req,res)=>{ register.handleRegister(req,res,db,bcrypt) } );
 app.get('/profile/:id',(req,res)=>{ profile.handleProfileGet(req,res,db) });
