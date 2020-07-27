d65995099d42cc4dda823d276c690e73a485eda4
aj709 <55025205+aj709@users.noreply.github.com>
2020-07-27 10:13:34 +0530

    adding port

--- server.js
+++ server.js
@@ -31,6 +31,6 @@ app.get('/profile/:id',(req,res)=>{ profile.handleProfileGet(req,res,db) });
 app.put('/images',(req,res)=>{ image.handleImage(req,res,db) });
 app.post('/imageurl',(req,res)=>{ image.handleApiCall(req,res) });
 
-app.listen(3000,()=>{
-	console.log("Working on 3000");
+app.listen(process.env.PORT || 3000,()=>{
+	console.log(`Working on ${process.env.PORT}`);
 });
